1d493e18cb88285d994755ce8635346606a14133
menmingfeng <menmingfeng@menmingfengMacBook-Pro.local>
2026-06-06 18:41:50 +0800

    适配Gitee Pages打包配置+完整源码

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig({base: '/menu-master/', 
   build: {
     sourcemap: 'hidden',
   },
